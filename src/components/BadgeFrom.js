c198df501acdb9af7d0378df62ead4e74a1939a7
Diseño UI/UX <Diseno@iMac-de-MJMAC-0002.local>
2019-05-29 14:12:17 -0500

    agregando formulario y estado compartido

--- src/components/BadgeFrom.js
+++ src/components/BadgeFrom.js
@@ -2,9 +2,6 @@ import React,  { Component } from 'react';
 
 class BadgeFrom extends Component{
 
-    handleChange = e => {
-        console.log({ value: e.target.value });
-    }
     handleClick = e => {
         console.log({
             value: e.target.value
@@ -12,7 +9,7 @@ class BadgeFrom extends Component{
     }
     handleSubmit = e =>{
         e.preventDefault();
-        console.log('from was submit')
+        console.log(this.props.formValues)
     }
 
     render(){
@@ -23,7 +20,48 @@ class BadgeFrom extends Component{
                 <form onSubmit={this.handleSubmit}>
                     <div className="form-group">
                         <label>Name</label>
-                        <input onChange={this.handleChange} className="form-control" type="text" name="fistname" />
+                            <input onChange={this.props.onChange} 
+                                className="form-control" 
+                                type="text"
+                                name="firstName"
+                                value={this.props.formValues.firstName}
+                            />
+                    </div>
+                    <div className="form-group">
+                        <label>Last Name</label>
+                            <input onChange={this.props.onChange} 
+                                className="form-control" 
+                                type="text"
+                                name="lastName"
+                                value={this.props.formValues.lastName}
+                            />
+                    </div>
+                    <div className="form-group">
+                        <label>Email</label>
+                            <input onChange={this.props.onChange} 
+                                className="form-control" 
+                                type="email"
+                                name="email"
+                                value={this.props.formValues.email}
+                            />
+                    </div>
+                    <div className="form-group">
+                        <label>Job Tile</label>
+                            <input onChange={this.props.onChange} 
+                                className="form-control" 
+                                type="text"
+                                name="jobTitle"
+                                value={this.props.formValues.jobTitle}
+                            />
+                    </div>
+                    <div className="form-group">
+                        <label>Twitter</label>
+                            <input onChange={this.props.onChange} 
+                                className="form-control" 
+                                type="text"
+                                name="twitter"
+                                value={this.props.formValues.twitter}
+                            />
                     </div>
                     <button type="submit" className="btn btn-primary" onClick={this.handleClick} >Save</button>
                 </form>
--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -8,6 +8,25 @@ import App from '../components/App' ;
 import BadgeForm from '../components/BadgeFrom';
 
 class BadgeNew extends Component{
+
+    state = {  
+        form: {
+            firstName: '',
+            lastName: '',
+            email: '',
+            jobTitle: '',
+            twitter: '',
+    } };
+    
+    hableChange = e => {
+        this.setState({
+            form : {
+                ...this.state.form,
+                [e.target.name]: e.target.value
+            },
+        });
+    };
+
     render(){
         return(
             <div>
@@ -26,7 +45,10 @@ class BadgeNew extends Component{
                                 />
                         </div>
                         <div className="col-6">
-                            <BadgeForm />
+                            <BadgeForm 
+                            onChange={this.hableChange.bind(this)}
+                            formValues={this.state.form}
+                            />
                         </div>
                     </div>
                 </div>
